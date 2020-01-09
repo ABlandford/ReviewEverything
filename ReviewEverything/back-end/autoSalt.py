6b1dd890e6c4c74f80de9378efa1bb6bd2e6a58d
Snowyorb <36518860+Snowyorb@users.noreply.github.com>
2020-01-09 09:51:09 -0700

    HashAndPass

--- ReviewEverything/back-end/autoSalt.py
+++ ReviewEverything/back-end/autoSalt.py
@@ -7,9 +7,17 @@ users = client.Users.User_Profiles
 
 
 for user in users.find({}, {"_id" :0, 'password' : ''}):
-    print(user)
+    global salt 
+    global hashed
+    # print(user['password'])
+    
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(user['password'].encode('utf8'), bcrypt.gensalt())
+    print(salt)
+    print(hashed)
+ 
 
+ 
 
-
-keganstuff = users.find_one({'fname' : 'Kegan'})
-print(keganstuff)
+# keganstuff = users.find_one({'fname' : 'Kegan'})
+# print(keganstuff)
