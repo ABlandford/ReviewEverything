d85c34b53130102c58b2b4831b3f15550df275dc
Austin Blandford <ablandford@student.neumont.edu>
2020-01-09 10:03:52 -0700

    IT WORKS
    IT REALLY FREAKING WORKS~!!!!!!!!!!!!!!!!!!!!!!!!!!!!!IJFPCAUDSBO8SUEI UVELUDGLIUF S VDLGUIZESBFVI

--- ReviewEverything/back-end/autoSalt.py
+++ ReviewEverything/back-end/autoSalt.py
@@ -5,11 +5,16 @@ client = MongoClient('localhost', 27017)
 
 users = client.data.users
 
-# for user in users.find({}, {"_id": 0, "password" : 1}):
-#     print(user)
+for user in users.find({}, {"_id": 0, "password" : 1}):
+    # print(user['password'])
+    hashed = bcrypt.hashpw(user['password'].encode('utf8'), bcrypt.gensalt())
+    # print(f'Hashed Password: {hashed}')
+    query = {"password" : f"{user['password']}"}
+    newPass = {"$set" : {"password" : f"{hashed}"}}
+    users.update_one(query, newPass)
 
-password = users.find_one({'password' : 'y]ed17gqv5'}, {"_id" : 0, "password" : 1})
-print(password)
+# password = users.find_one({'password' : 'y]ed17gqv5'}, {"_id" : 0, "password" : 1})
+# print(password)
 
-hashed = bcrypt.hashpw(password['password'].encode('utf8'), bcrypt.gensalt())
-print(hashed)
+# hashed = bcrypt.hashpw(password['password'].encode('utf8'), bcrypt.gensalt())
+# print(hashed)
