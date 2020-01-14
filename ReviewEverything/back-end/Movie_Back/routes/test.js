452604cf56ef2f95a08c7c3de2db72a3b1dd559e
Snowyorb <36518860+Snowyorb@users.noreply.github.com>
2020-01-14 09:59:42 -0700

    Test

--- ReviewEverything/back-end/Movie_Back/routes/test.js
+++ ReviewEverything/back-end/Movie_Back/routes/test.js
@@ -1,6 +1,19 @@
 
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("Users");
+  dbo.collection("fname").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    console.log("BRUH")
+    db.close();
+  });
+});
 
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
