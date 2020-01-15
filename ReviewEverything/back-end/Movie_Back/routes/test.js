3ff393739595da5f9e88c42fab79ca410c5dc8a0
Snowyorb <36518860+Snowyorb@users.noreply.github.com>
2020-01-15 10:06:46 -0700

    ok

--- ReviewEverything/back-end/Movie_Back/routes/test.js
+++ ReviewEverything/back-end/Movie_Back/routes/test.js
@@ -1,22 +1,35 @@
-
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://localhost:27017/Users/User_Profiles";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("Users");
-  dbo.collection("fname").findOne({}, function(err, result) {
-    if (err) throw err;
-    console.log(result.name);
-    console.log("BRUH")
-    db.close();
-  });
-});
+  var f = dbo.collection("fname").findOne({'fname' : 'Bob'}
+//     if (err) throw err;
+//     console.log(result);
+//     console.log("BRUH")
+//     db.close();
+//   });
+ );
+ console.log(f)
+})
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//   var dbo = db.db("Users");
+//   dbo.collection('fname').findOne({"fname":'Bob'})
+//     .then(function(result) {
+//       console.log(result); // Use this to debug
+//       callback(result);
+//     })
+// });
+
+
 
 router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+    res.send('FHjdsfshjfhjds'); 
 });
 
-module.exports = router;
+module.exports = MongoClient; 
